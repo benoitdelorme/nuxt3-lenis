ab68c7e63c81401343a992a29c0143afbb22c2d3
HaileabT <haileabtesfaye11@gmail.com>
2025-03-24 06:44:39 -0400

    IMROVEMENT: updated used packages

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
-  myModule: {},
-  devtools: { enabled: true }
-})
+  modules: ["../src/module"],
+  devtools: { enabled: true },
+  compatibilityDate: "2025-03-22",
+});
